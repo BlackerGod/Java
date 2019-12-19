bb6701fd3b1d6c64bc985ad9057436365ed628ac
YinYang <949492541@qq.com>
2019-12-19 10:58:09 +0800

    2019/12/19

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Person xiaohua=new Person(18,"二狗");
+        xiaohua.eat();
+    }
+}
+
+
---
+++ HelloWorld/src/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public void eat(){
+        System.out.println(name+"吃饭");
+    }
+
+}
