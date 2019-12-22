2ea11394a3a4ae87c75647a7e472b740659b31c1
YinYang <949492541@qq.com>
2019-12-22 17:52:56 +0800

    2019/12/22

---
+++ Homework7/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    private String name;
+    private int age;
+    private String sex;
+    private long number;
+    static int sequence=0;
+    public Test(String name, int age, String sex, long number) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.number = number;
+    }
+
+}
